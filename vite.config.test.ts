8d3b5180db281c336793174305d783e1afa6d5e0
Oanh Nguyen <oanhnn.bk@gmail.com>
2021-03-21 23:26:27 +0700

    Setup tests

---
+++ vite.config.test.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+import vue from '@vitejs/plugin-vue'
+import viteTestPlugin from 'vite-plugin-test'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    viteTestPlugin(),
+  ]
+})
